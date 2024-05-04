b74594bddb379ac2c32c0ffedc0324904e845b19
Arkarin0 <snaut@hotmail.de>
2024-05-04 21:49:21 +0200

    finished part 7 of the tutorial

--- run.cmd
+++ run.cmd
@@ -1,1 +1,9 @@
-docker run -dp 127.0.0.1:3000:3000 --mount type=volume,src=todo-db,target=/etc/todos getting-started
+docker run -dp 127.0.0.1:3000:3000 ^
+  -w /app -v "%cd%:/app" ^
+  --network todo-app ^
+  -e MYSQL_HOST=mysql ^
+  -e MYSQL_USER=root ^
+  -e MYSQL_PASSWORD=secret ^
+  -e MYSQL_DB=todos ^
+  node:18-alpine ^
+  sh -c "yarn install && yarn run dev"
