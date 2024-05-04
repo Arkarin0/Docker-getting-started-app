b8f65499e4a90a55c54a439588db5d759e8d425f
Arkarin0 <snaut@hotmail.de>
2024-05-04 22:01:54 +0200

    finished part 8 of the tutorial.

--- run.cmd
+++ run.cmd
@@ -1,9 +1,1 @@
-docker run -dp 127.0.0.1:3000:3000 ^
-  -w /app -v "%cd%:/app" ^
-  --network todo-app ^
-  -e MYSQL_HOST=mysql ^
-  -e MYSQL_USER=root ^
-  -e MYSQL_PASSWORD=secret ^
-  -e MYSQL_DB=todos ^
-  node:18-alpine ^
-  sh -c "yarn install && yarn run dev"
+docker compose up -d
